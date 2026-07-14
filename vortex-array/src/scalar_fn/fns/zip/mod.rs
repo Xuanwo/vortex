d10d3fd77464bc8d49b4aff0b28561b4365965ac
Robert Kruszewski <github@robertk.io>
2026-07-14 16:33:28 +0100

    fix
    Signed-off-by: Robert Kruszewski <github@robertk.io>

--- vortex-array/src/scalar_fn/fns/zip/mod.rs
+++ vortex-array/src/scalar_fn/fns/zip/mod.rs
@@ -492,8 +492,7 @@ mod tests {
             .unwrap()
             .execute::<ArrayRef>(&mut ctx)
             .unwrap();
-        let zipped = zipped.as_opt::<VarBinView>().unwrap();
-        assert_eq!(zipped.data_buffers().len(), 2);
+        assert_eq!(zipped.as_::<VarBinView>().data_buffers().len(), 2);
 
         let true_value = |i: usize| {
             if i.is_multiple_of(2) {
@@ -516,6 +515,6 @@ mod tests {
                 false_value(i)
             }
         }));
-        assert_arrays_eq!(zipped.materialize_array_ref().clone(), expected, &mut ctx);
+        assert_arrays_eq!(zipped, expected, &mut ctx);
     }
 }
